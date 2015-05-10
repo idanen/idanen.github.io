bfaaa59bbc0c68cb19f5d9bfe5c901f362427252
Idan Entin <idanen@gmail.com>
2015-05-10 23:14:53 +0300

    Refactoring

--- app/scripts/manager/player-details-drtv.js
+++ app/scripts/manager/player-details-drtv.js
@@ -1,9 +1,73 @@
+(function () {
 /**
  * The managed game's directive
  */
 angular.module( 'pokerManager' ).
-	directive( 'playerDetails', [ '$filter', '$timeout', 'Players', 'Stats', function( $filter, $timeout, Players, stats ) {
+	directive( 'playerDetails', playerDetailsDirective );
+
+	playerDetailsDirective.$inject = [ '$filter', '$timeout', 'Players', 'Stats' ];
+
+	function playerDetailsDirective( $filter, $timeout, Players, stats ) {
 		'use strict';
+
+		return {
+			restrict: 'AE',
+			scope: {
+				player: '='
+			},
+			controller: 'PlayerDetailsCtrl',
+			templateUrl: 'partials/tmpls/player-details-tmpl.html',
+			link: postLinkFn
+		};
+
+		function postLinkFn( scope, element, attrs, ctrl ) {
+			var chartData = createData( scope.player ),
+				chartHolder = angular.element('<div/>'),
+				chartObj = createChartObject( scope.player.name, chartData ),
+				refreshBtn = element.find( '.refresh-data-btn' );
+
+			function refreshData() {
+				scope.loading = false;
+
+				// Calculate extra data
+				scope.player.winningSessions = winningSessions( scope.player );
+				scope.player.avgWinning = avgWinning( scope.player, true );
+
+				chartData = createData( scope.player );
+
+				chartHolder.highcharts().destroy();
+
+				updateChartData( chartObj, chartData );
+
+				// Defer the chart build so that it could take the parent's width (the parent is hidden until $digest will be done to update the ng-show)
+				$timeout( function () {
+					chartHolder.highcharts( chartObj );
+				}, 0, false);
+			}
+		
+			// Create a placeholder for the chart
+			refreshBtn.after( chartHolder );
+			chartHolder.attr( 'id', 'player-chart' );
+
+			// Construct chart on next $digest loop so the chart container will fill width
+			$timeout( function () {
+				chartHolder.highcharts( chartObj );
+			}, 0, false);
+
+			if ( !scope.player.isNew ) {
+				scope.player = Players.get( { playerId: scope.player.id }, refreshData );
+			}
+			
+			refreshBtn.on( 'click', refreshData );
+
+			scope.$on( '$destroy', function () {
+				if ( chartHolder ) {
+					chartHolder.highcharts().destroy();
+					chartHolder.remove();
+				}
+				refreshBtn.off();
+			} );
+		}
 
 		function createData( player ) {
 			var chartData = {
@@ -105,61 +169,5 @@ angular.module( 'pokerManager' ).
 			chartObj.series[ 0 ].data = newData.profits;
 			chartObj.series[ 1 ].data = newData.balances;
 		}
-
-		return {
-			restrict: 'AE',
-			scope: {
-				player: '='
-			},
-			controller: 'PlayerDetailsCtrl',
-			templateUrl: 'partials/tmpls/player-details-tmpl.html',
-			link: function( scope, element, attrs, ctrl ) {
-				var chartData = createData( scope.player ),
-					chartHolder = angular.element('<div/>'),
-					chartObj = createChartObject( scope.player.name, chartData ),
-					refreshBtn = element.find( '.refresh-data-btn' );
-
-				function refreshData() {
-					scope.loading = false;
-
-					// Calculate extra data
-					scope.player.winningSessions = winningSessions( scope.player );
-					scope.player.avgWinning = avgWinning( scope.player, true );
-
-					chartData = createData( scope.player );
-
-					chartHolder.highcharts().destroy();
-
-					updateChartData( chartObj, chartData );
-
-					// Defer the chart build so that it could take the parent's width (the parent is hidden until $digest will be done to update the ng-show)
-					$timeout( function () {
-						chartHolder.highcharts( chartObj );
-					}, 0, false);
-				}
-			
-				// Create a placeholder for the chart
-				refreshBtn.after( chartHolder );
-				chartHolder.attr( 'id', 'player-chart' );
-
-				// Construct chart on next $digest loop so the chart container will fill width
-				$timeout( function () {
-					chartHolder.highcharts( chartObj );
-				}, 0, false);
-
-				if ( !scope.player.isNew ) {
-					scope.player = Players.get( { playerId: scope.player.id }, refreshData );
-				}
-				
-				refreshBtn.on( 'click', refreshData );
-
-				scope.$on( '$destroy', function () {
-					if ( chartHolder ) {
-						chartHolder.highcharts().destroy();
-						chartHolder.remove();
-					}
-					refreshBtn.off();
-				} );
-			}
-		};
-	} ] );
+	}
+})();

b7689f2dfc92ad5f4b0087637bfab725fb76f5d6
Idan Entin <idanen@gmail.com>
2015-11-05 17:23:07 +0200

    [feature ui-router]: Starting ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('pokerManager', ['ngRoute', 'ngAnimate', 'angulartics', 'angulartics.google.analytics',
+    angular.module('pokerManager', ['ngRoute', 'ui.router', 'ngAnimate', 'angulartics', 'angulartics.google.analytics',
         'ui.bootstrap', 'mgcrea.ngStrap', 'toaster', 'firebase', 'firebase.ref',
         'firebase.auth', 'jackrabbitsgroup.angular-google-auth', 'directive.g+signin', 'pokerManager.filters',
         'pokerManager.services', 'pokerManager.directives', 'pokerManager.controllers'])

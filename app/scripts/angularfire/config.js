468bdd922e443993e91e0f75dbc13509cb44b728
Idan Entin <idanen@gmail.com>
2016-06-24 22:13:49 +0300

    Config Firebase in `run` block

--- app/scripts/angularfire/config.js
+++ app/scripts/angularfire/config.js
@@ -6,18 +6,18 @@
     .constant('FB_APIKEY', 'AIzaSyDMrP9xSV8woT6-lt-TBiyMkSy0r3EiHAs')
     .constant('SIMPLE_LOGIN_PROVIDERS', ['password', 'anonymous', 'facebook', 'google'])
     .constant('GOOGLE_AUTH_SCOPES', ['profile', 'email', 'https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/plus.profile.emails.read'])
-    .constant('loginRedirectPath', '/login')
-    .config(firebaseConfig);
-
-  firebaseConfig.$inject = ['$windowProvider', 'FBURL', 'FB_APIKEY'];
-  function firebaseConfig($windowProvider, FBURL, FB_APIKEY) {
-    // Initialize Firebase
-    var config = {
-      apiKey: FB_APIKEY,
+    .constant('FIREBASE_CONFIG', {
+      apiKey: 'AIzaSyDMrP9xSV8woT6-lt-TBiyMkSy0r3EiHAs',
       authDomain: 'fiery-heat-6939.firebaseapp.com',
-      databaseURL: FBURL,
+      databaseURL: 'https://fiery-heat-6939.firebaseio.com',
       storageBucket: 'fiery-heat-6939.appspot.com'
-    };
-    $windowProvider.$get().firebase.initializeApp(config);
+    })
+    .constant('loginRedirectPath', '/login')
+    .run(firebaseConfig);
+
+  firebaseConfig.$inject = ['$window', 'FIREBASE_CONFIG'];
+  function firebaseConfig($window, FIREBASE_CONFIG) {
+    // Initialize Firebase
+    $window.firebase.initializeApp(FIREBASE_CONFIG);
   }
 }());

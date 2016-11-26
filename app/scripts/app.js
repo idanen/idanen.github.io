526341db54d076a7698711fe129bf71b8614938e
Idan Entin <idanen@gmail.com>
2016-11-26 23:30:31 +0200

    Load improvements (WIP)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,7 @@
   angular.module('pokerManager', ['ui.router', 'ngAnimate', 'angulartics', 'angulartics.google.analytics',
     'ui.bootstrap', 'mgcrea.ngStrap', 'firebase', 'firebase.ref', 'pushState', 'ngclipboard',
     'pokerManager.filters', 'pokerManager.services', 'pokerManager.controllers'])
-    .constant('BASE_URL', {
-      DEV: 'http://localhost:9880/services/',
-      PROD: 'https://awesome-sphere-397.appspot.com/services/'
-    });
+    .config(['$compileProvider', $compileProvider => {
+      $compileProvider.debugInfoEnabled(false);
+    }]);
 }());
